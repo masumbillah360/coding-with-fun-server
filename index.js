d4521a2156fbb283713d36e09fa58922afb97a4a
Masum Billah <masumbillah6584@gmail.com>
2022-10-25 09:36:54 +0600

    added courese and categories data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+const courses = require('./data/courses.json');
+const categories = require('./data/categories.json');
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Server is Running...');
+});
+
+app.get('/courses', (req, res) => {
+    res.json(courses);
+});
+
+app.get('/categories', (req, res) => {
+    res.json(categories);
 });
 
 app.listen(port, () => {
